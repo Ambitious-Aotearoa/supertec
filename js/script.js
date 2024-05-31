e850e96d436ca65e727ea3951d022b9739ae40b2
jennifer <jennifer@ambitious.co.nz>
2024-05-31 13:57:58 +1200

    base nav added

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 let navLinks = document.querySelector('.desktop-nav')
 let menuOpenBtn = document.querySelector('.hamburger')
 
-// Open nav, toggle hamburger and add non scroll to body
+////On toggleMenu, change navLinks top position, to show or hide nav links dropdown
 function toggleMenu() {
-  if (navLinks.style.top === '70px') {
-    navLinks.style.top = '-100vh' // Close the menu
+  if (navLinks.classList.contains('active')) {
+    navLinks.classList.remove('active')
   } else {
-    navLinks.style.top = '70px' // Open the menu
+    navLinks.classList.add('active')
   }
 }
 menuOpenBtn.addEventListener('click', toggleMenu)
 
-// Close the menu, if the user touches outside of it
+//////Close the menu, if the user touches outside of it
 function closeMenu(event) {
   if (!navLinks.contains(event.target) && !menuOpenBtn.contains(event.target)) {
-    navLinks.style.top = '-100vh' // Close the menu
+    navLinks.classList.remove('active')
   }
 }
 document.addEventListener('touchstart', closeMenu)
 document.addEventListener('click', closeMenu)
 
-// Add event listener to close menu when an anchor inside the nav is clicked
+//////Add event listener to close menu when an anchor inside the nav is clicked
 let navAnchors = navLinks.querySelectorAll('a')
 
 navAnchors.forEach((anchor) => {
   anchor.addEventListener('click', function (event) {
     event.preventDefault()
 
-    if (navLinks.style.top === '70px') {
-      // Only close if the menu is open
-      navLinks.style.top = '-100vh' // Close the menu
-    }
-
     // Smooth scroll with offset
     //substring removes hash
     let targetId = this.getAttribute('href').substring(1)
     let targetElement = document.getElementById(targetId)
+
+    // Remove active class from navLinks/close nav
+    navLinks.classList.remove('active')
 
     setTimeout(() => {
       window.scrollTo({
@@ -46,24 +44,18 @@ navAnchors.forEach((anchor) => {
   })
 })
 
-// Function to toggle the display of .mobile-nav based on window width
+//if hamburger toggle is open and user changes screen width, greater than 768px, hamburger open toggle/ active state removed.
+//Desktop nav ccs is active.
 function toggleMobileNav() {
   if (window.innerWidth > 768) {
     console.log('desktop')
-    if (navLinks.style.top === '70px') {
-      navLinks.style.top = '0' // Reset the top position for desktop view
-    }
+    navLinks.classList.remove('active')
   } else {
     console.log('mobile')
-    if (navLinks.style.top === '0px') {
-      navLinks.style.top = '70px' // Reset the top position for desktop view
-    }
   }
 }
-
 // Initial check
 toggleMobileNav()
-
 // Listen for window resize and orientation change events
 window.addEventListener('resize', toggleMobileNav)
 window.addEventListener('orientationchange', toggleMobileNav)
