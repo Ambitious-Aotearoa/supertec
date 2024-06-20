1ef20dcd3865f61e7a4610a63c434650d244fef3
jennifer <jennifer@ambitious.co.nz>
2024-06-21 11:08:35 +1200

    finished

--- js/gsap.js
+++ js/gsap.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   gsap.registerPlugin(ScrollTrigger, SplitText)
+  ScrollTrigger.refresh()
+
+  ScrollTrigger.config({
+    autoRefreshEvents: 'visibilitychange,DOMContentLoaded,load, resize',
+  })
+
   var marker = { startColor: 'green', endColor: 'red', fontSize: '18px', fontWeight: 'bold', indent: 20, zIndex: 2000 }
 
   //white text on black background
@@ -164,6 +170,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
     tl.from('.dots', { duration: 1.2, xPercent: -50, yPercent: 50 }, 'start')
   }
 
+  // Marquee effect
+  function marquee() {
+    const marquee = document.querySelectorAll('.marquee')
+    marquee.forEach((e) => {
+      // Create swiper carousel
+      const carousel = e.querySelectorAll('.marquee-carousel')
+
+      carousel.forEach((e) => {
+        const items = e.querySelector('.marquee-items')
+        const item = e.querySelectorAll('.marquee-item')
+
+        e.classList.add('swiper-container')
+        items.classList.add('swiper-wrapper')
+        item.forEach((e) => e.classList.add('swiper-slide'))
+
+        const slider = new Swiper(e, {
+          slidesPerView: 'auto',
+          loop: true,
+          freeMode: true,
+          freeModeMomentumBounce: false,
+          freeModeMomentumVelocityRatio: 0.3,
+        })
+      })
+
+      // Scroll triggered movement
+      const tl = new gsap.timeline()
+
+      tl.set(carousel, { willChange: 'transform' })
+      tl.fromTo(carousel[0], { x: -300 }, { x: 0, ease: 'none' }, 0)
+      tl.fromTo(carousel[1], { x: 300 }, { x: 0, ease: 'none' }, 0)
+      tl.set(carousel, { willChange: 'auto' })
+
+      ScrollTrigger.create({
+        trigger: e,
+        animation: tl,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: 0.3,
+        refreshPriority: -14,
+      })
+    })
+  }
+
   // Check if the screen width is at least 768px
   if (window.matchMedia('(min-width: 768px)').matches) {
     animateDesktop()
@@ -173,5 +222,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     circle()
     animateDesktopDarkH1()
     hero()
+    marquee()
   }
 })
